e24dcbe7c2e2a3cee6ee285e97141cd0f44fc7ee
MueR <bas@muer.nl>
2021-11-30 23:25:11 +0100

    Initial commit

---
+++ src/AbstractSolver.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode;
+
+use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Stopwatch\StopwatchEvent;
+
+abstract class AbstractSolver
+{
+    private Stopwatch $stopwatch;
+    protected string|array $input;
+    private string $ns;
+    public int $day;
+
+    public function __construct()
+    {
+        $this->ns = substr(substr(get_class($this), 0, strrpos(get_class($this), '\\')), -5);
+        $this->day = (int) substr($this->ns, -2);
+        $this->stopwatch = new Stopwatch(true);
+        $this->stopwatch->start($this->ns);
+        $this->readInput();
+    }
+
+    abstract public function partOne(): int;
+
+    abstract public function partTwo(): int;
+
+    public function lap(): StopwatchEvent
+    {
+        return $this->stopwatch->lap($this->ns);
+    }
+
+    public function stop(): StopwatchEvent
+    {
+        return $this->stopwatch->stop($this->ns);
+    }
+
+    protected function readInput(): void
+    {
+        $this->input = require __DIR__ . '/' . $this->ns . '/input.php';
+    }
+}
