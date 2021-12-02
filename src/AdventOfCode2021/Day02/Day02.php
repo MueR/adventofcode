ae9d35a2d89f539520ecb3d564674af6cc99f7ba
Bas <bas@muer.nl>
2021-12-02 09:07:10 +0100

    Day 2 solved.

---
+++ src/AdventOfCode2021/Day02/Day02.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode\AdventOfCode2021\Day02;
+
+use JetBrains\PhpStorm\Pure;
+use MueR\AdventOfCode\AbstractSolver;
+
+class Day02 extends AbstractSolver
+{
+    private int $aim = 0;
+    private int $depth = 0;
+    private int $position = 0;
+
+    public function partOne(): int
+    {
+        $this->navigate();
+
+        return $this->aim * $this->position;
+    }
+
+    public function partTwo(): int
+    {
+        return $this->depth * $this->position;
+    }
+
+    private function navigate(): void
+    {
+        foreach ($this->input as $line) {
+            if ($line === '') {
+                continue;
+            }
+            preg_match('/^(?<direction>up|down|forward) (?<units>\d+)$/im', $line, $instruction);
+            switch ($instruction['direction']) {
+                case 'forward':
+                    $this->position += $instruction['units'];
+                    $this->depth += $this->aim * $instruction['units'];
+                    break;
+                case 'up':
+                    $this->aim -= $instruction['units'];
+                    break;
+                case 'down':
+                    $this->aim += $instruction['units'];
+                    break;
+            }
+        }
+    }
+}
+
