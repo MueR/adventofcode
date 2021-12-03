b608df934a9e69b8f43bdc3f53d8b48c375b7d29
Bas <bas@muer.nl>
2021-12-03 13:03:19 +0100

    Day 3.

---
+++ src/AdventOfCode2021/Day03/Day03.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode\AdventOfCode2021\Day03;
+
+use MueR\AdventOfCode\AbstractSolver;
+use RuntimeException;
+
+class Day03 extends AbstractSolver
+{
+    protected array $testInput = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010'];
+
+    public function partOne(): int
+    {
+        $gamma = $epsilon = '';
+        for ($i = 0, $l = strlen($this->input[0]); $i < $l; $i++) {
+            $values = [0 => 0, 1 => 0];
+            foreach ($this->input as $bit) {
+                $values[((int)$bit[$i])]++;
+            }
+            $gamma .= $values[0] > $values[1] ? 0 : 1;
+            $epsilon .= $values[0] < $values[1] ? 0 : 1;
+        }
+        return bindec($gamma) * bindec($epsilon);
+    }
+
+    public function partTwo(): int
+    {
+        $oxygen = $this->findValue($this->input, true);
+        $co2 = $this->findValue($this->input, false);
+
+        return bindec($oxygen) * bindec($co2);
+    }
+
+    public function findValue(array $input, bool $mostCommon): ?string
+    {
+        for ($i = 0, $l = strlen($input[0]); $i < $l; $i++) {
+            $input = $this->reduce($input, $i, $mostCommon);
+            if (count($input) === 1) {
+                return array_shift($input);
+            }
+        }
+
+        throw new RuntimeException('No valid combination found.');
+    }
+
+    public function reduce(array $input, int $position, bool $mostCommon): array
+    {
+        $search = $mostCommon ? '1' : '0';
+        $matching = 2 * count(array_filter($input, static fn ($line) => $line[$position] === $search));
+        $keepLine = $search;
+        if ($matching !== count($input)) {
+            $keepLine = $matching > count($input) ? '1' : '0';
+        }
+
+        return array_filter($input, static fn ($line) => $line[$position] === $keepLine);
+    }
+}
+
