6e661d376c6f9bba1395a8a51f49e463a5284881
MueR <bas@muer.nl>
2021-12-08 14:35:26 +0100

    Optimized the order in which tests are run

--- src/AdventOfCode2021/Day08/Day08.php
+++ src/AdventOfCode2021/Day08/Day08.php
@@ -51,33 +51,34 @@ class Day08 extends AbstractSolver
         $this->signalsPerDigit = array_fill_keys(range(0, 9), '');
         $tests = [
             // 1 is the only one with 2 segments
-            '1' => fn (string $pattern) => strlen($pattern) === 2,
+            '1' => static fn (string $pattern) => strlen($pattern) === 2,
             // 7 is the only one with 3 segments
-            '7' => fn (string $pattern) => strlen($pattern) === 3,
+            '7' => static fn (string $pattern) => strlen($pattern) === 3,
             // 4 is the only one with 4 segments
-            '4' => fn (string $pattern) => strlen($pattern) === 4,
+            '4' => static fn (string $pattern) => strlen($pattern) === 4,
             // 8 is the only one with 7 segments
-            '8' => fn (string $pattern) => strlen($pattern) === 7,
-            // 9 matches segments for 4 and 7 (5 segments) plus 1 extra
-            '9' => fn (string $pattern) => strlen($pattern) === 6 && similar_text($pattern, $this->sortPattern($this->signalsPerDigit[4] . $this->signalsPerDigit[7])) === 5,
-            // 0 is 6 segments, matching 7's segments (9 is already out)
-            '0' => fn (string $pattern) => strlen($pattern) === 6 && similar_text($pattern, $this->signalsPerDigit[7]) === 3,
+            '8' => static fn (string $pattern) => strlen($pattern) === 7,
+            // 9 is 6 segments, matches segments for 4
+            '9' => fn (string $pattern) => strlen($pattern) === 6 && $this->matchesAll($pattern, $this->signalsPerDigit[4]),
+            // 0 is 6 segments, matching 1's segments (9 is already out)
+            '0' => fn (string $pattern) => strlen($pattern) === 6 && $this->matchesAll($pattern, $this->signalsPerDigit[1]),
+            // 6 is 6 segments, the only one left
+            '6' => static fn (string $pattern) => strlen($pattern) === 6,
             // 3 is 5 segments and matches 1's segments
-            '3' => fn (string $pattern) => strlen($pattern) === 5 && similar_text($pattern, $this->signalsPerDigit[1]) === 2,
-            // 5 is 5 segments, matching 3 of 4 segments with 4.
-            '5' => fn (string $pattern) => strlen($pattern) === 5 && similar_text($pattern, $this->signalsPerDigit[4]) === 3,
-            // 2 is the only one remaining with length 5
-            '2' => fn (string $pattern) => strlen($pattern) === 5,
-            // aaaaand that just leaves 6.
-            '6' => fn (string $pattern) => true,
+            '3' => fn (string $pattern) => strlen($pattern) === 5 && $this->matchesAll($pattern, $this->signalsPerDigit[1]),
+            // 5 is 5 segments, and 9 has all the segments of 5
+            '5' => fn (string $pattern) => strlen($pattern) === 5 && $this->matchesAll($this->signalsPerDigit[9], $pattern),
+            // 2 is the only one remaining
+            '2' => static fn (string $pattern) => true,
         ];
         foreach ($tests as $digit => $test) {
             foreach ($input as $key => $pattern) {
-               if ($test($pattern)) {
-                   unset($input[$key]);
-                   $this->signalsPerDigit[$digit] = $pattern;
-                   break;
-               }
+                if (!$test($pattern)) {
+                    continue;
+                }
+                unset($input[$key]);
+                $this->signalsPerDigit[$digit] = $pattern;
+                break;
             }
         }
     }
@@ -87,6 +88,17 @@ class Day08 extends AbstractSolver
         $letters = array_unique(str_split($pattern));
         sort($letters);
         return implode('', $letters);
+    }
+
+    protected function matchesAll(string $one, string $two): bool
+    {
+        for ($i = 0, $l = strlen($two); $i < $l; $i++) {
+            if (!str_contains($one, $two[$i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     protected function parse(): void
