2fe9f9eb0fcec5e3cea940529f44e67934180df9
MueR <bas@muer.nl>
2021-12-11 17:44:02 +0100

    Day 11 not working

---
+++ src/AdventOfCode2021/Day11/Day11.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode\AdventOfCode2021\Day11;
+
+use MueR\AdventOfCode\AbstractSolver;
+
+/**
+ * Day 11 puzzle.
+ *
+ * @property array{int} $input
+ */
+class Day11 extends AbstractSolver
+{
+    /** @var array<int, array<int, Octopus>> */
+    private array $octopuses = [];
+
+    public function partOne(): int
+    {
+        printf("Before, energy %d\n", $this->octopuses[0][0]->energyLevel);
+        $this->printGrid();
+        $result = 0;
+        for ($i = 0; $i < 2; $i++) {
+            $result += $this->step();
+        }
+        return $result;
+    }
+
+    public function partTwo(): int
+    {
+        return -1;
+    }
+
+    protected function step(): int
+    {
+        $this->walk(fn (Octopus $octopus) => $octopus->increase());
+        $this->walk(function (Octopus $octopus) {
+            if ($octopus->energyLevel > 9 && !$octopus->hasFlashed) {
+                $octopus->flash();
+            }
+        });
+        $result = $this->walk(fn (Octopus $octopus) => $octopus->flashed());
+        $this->printGrid();
+
+        return count($result);
+    }
+
+    protected function walk(callable $callable): mixed
+    {
+        $return = [];
+        foreach ($this->octopuses as $x => $octopuses) {
+            foreach ($octopuses as $y => $octopus) {
+                $return[] = $callable($octopus);
+            }
+        }
+
+        return array_filter($return);
+    }
+
+    protected function parse(): void
+    {
+        $lines = explode("\n", $this->readText());
+        foreach ($lines as $x => $line) {
+            foreach (str_split($line) as $y => $energy) {
+                $this->octopuses[$x][$y] = new Octopus($x, $y, (int) $energy);
+            }
+        }
+
+        $this->walk(fn (Octopus $octopus) => $octopus->findNeighbours($this->octopuses));
+    }
+
+    protected function printGrid(): void
+    {
+        foreach ($this->octopuses as $x => $octopuses) {
+            foreach ($octopuses as $y => $octopus) {
+                print $octopus->energyLevel;
+            }
+            print "\n";
+        }
+        print "\n";
+    }
+}
+
+class Octopus
+{
+    /** @var Octopus[] */
+    public array $neighbours = [];
+
+    public function __construct(public int $x, public int $y, public int $energyLevel, public bool $hasFlashed = false)
+    {
+    }
+
+    public function increase(): void
+    {
+        $this->energyLevel++;
+    }
+
+    public function flash(): void
+    {
+        $this->energyLevel = 0;
+        $this->hasFlashed = true;
+        foreach($this->neighbours as $neighbour) {
+            $neighbour->increase();
+        }
+        // printf("Octopus %d %d flashed (EL: %d)!\n", $this->x, $this->y, $this->energyLevel);
+    }
+
+    public function flashed(): ?int
+    {
+        if ($this->hasFlashed || $this->energyLevel > 9) {
+            $this->energyLevel = 0;
+            $this->hasFlashed = false;
+            return 1;
+        }
+        $this->hasFlashed = false;
+        return null;
+    }
+
+    public function findNeighbours(array $grid): void
+    {
+        $deltas = [[-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [-1, 1], [1, 1]];
+        foreach ($deltas as $delta) {
+            $findX = $this->x + $delta[0];
+            $findY = $this->y + $delta[1];
+            if (!array_key_exists($findX, $grid)) {
+                continue;
+            }
+            if (!array_key_exists($findY, $grid[$findX])) {
+                continue;
+            }
+            $this->neighbours[] = $grid[$findX][$findY];
+        }
+    }
+}
+
