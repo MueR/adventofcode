c47d733829234c26bfd85affb70c66e570833205
MueR <bas@muer.nl>
2022-12-04 11:01:22 +0100

    Day 3 & 4

---
+++ src/AdventOfCode2022/Day03/Day03.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Part of AdventOfCode 2022
+ */
+
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode\AdventOfCode2022\Day03;
+
+use MueR\AdventOfCode\AbstractSolver;
+
+/**
+ * Day 3 puzzle.
+ *
+ * @property array{int} $input
+ * @see https://adventofcode.com/2022/day/3
+ */
+class Day03 extends AbstractSolver
+{
+    /**
+     * Solver method for part 1 of the puzzle.
+     *
+     * @see https://adventofcode.com/2022/day/3
+     */
+    public function partOne(): int
+    {
+        $sum = 0;
+        foreach ($this->input as $line) {
+            $compSize = strlen($line) / 2;
+            [$compOne, $compTwo] = str_split($line, $compSize);
+            $items = array_unique(str_split($compOne));
+            foreach ($items as $item) {
+                if (str_contains($compTwo, $item)) {
+                    $sum += $this->getPriority($item);
+                }
+            }
+        }
+
+        return $sum;
+    }
+
+    /**
+     * Solver method for part 2 of the puzzle.
+     *
+     * @see https://adventofcode.com/2022/day/3#part2
+     */
+    public function partTwo(): int
+    {
+        $sum = 0;
+        for ($i = 0, $max = count($this->input); $i < $max; $i += 3) {
+            $lineOne = str_split($this->input[$i]);
+            $lineTwo = $this->input[$i+1];
+            $lineThree = $this->input[$i+2];
+
+            foreach ($lineOne as $item) {
+                if (str_contains($lineTwo, $item) && str_contains($lineThree, $item)) {
+                    $sum += $this->getPriority($item);
+                    break;
+                }
+            }
+        }
+        return $sum;
+    }
+
+    protected function parse(): void
+    {
+        $this->input = explode("\n", $this->readText());
+    }
+
+    private function getPriority(string $item): int
+    {
+        $code = ord($item);
+        if ($code >= ord('a')) {
+            // lowercase
+            return $code - ord('a') + 1;
+        }
+        return $code - ord('A') + 27;
+    }
+}
---
+++ src/AdventOfCode2022/Day04/Day04.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Part of AdventOfCode 2022
+ */
+
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode\AdventOfCode2022\Day04;
+
+use MueR\AdventOfCode\AbstractSolver;
+
+/**
+ * Day 4 puzzle.
+ *
+ * @property array{int} $input
+ * @see https://adventofcode.com/2022/day/4
+ */
+class Day04 extends AbstractSolver
+{
+    /**
+     * Solver method for part 1 of the puzzle.
+     *
+     * @see https://adventofcode.com/2022/day/4
+     */
+    public function partOne(): int
+    {
+        $fullOverlap = 0;
+        foreach ($this->input as $index => [$elfOne, $elfTwo]) {
+            if (
+                ($elfOne[0] >= $elfTwo[0] && $elfOne[1] <= $elfTwo[1]) ||
+                ($elfTwo[0] >= $elfOne[0] && $elfTwo[1] <= $elfOne[1])
+            ) {
+                $fullOverlap++;
+            }
+        }
+        return $fullOverlap;
+    }
+
+    /**
+     * Solver method for part 2 of the puzzle.
+     *
+     * @see https://adventofcode.com/2022/day/4#part2
+     */
+    public function partTwo(): int
+    {
+        $partialOverlap = 0;
+        foreach ($this->input as [$elfOne, $elfTwo]) {
+            if (
+                ($elfOne[0] >= $elfTwo[0] && $elfOne[0] <= $elfTwo[1]) ||
+                ($elfOne[1] >= $elfTwo[0] && $elfOne[1] <= $elfTwo[1]) ||
+                ($elfTwo[0] >= $elfOne[0] && $elfTwo[0] <= $elfOne[1]) ||
+                ($elfTwo[1] >= $elfOne[0] && $elfTwo[1] <= $elfOne[1])
+            ) {
+                $partialOverlap++;
+            }
+        }
+        return $partialOverlap;
+    }
+
+    protected function parse(): void
+    {
+        $this->input = array_map(
+            static function (string $line) {
+                [$elfOne, $elfTwo] = explode(',', $line);
+                $elfOne = array_map(static fn ($number) => (int)$number, explode('-', $elfOne));
+                $elfTwo = array_map(static fn ($number) => (int)$number, explode('-', $elfTwo));
+                return [$elfOne, $elfTwo];
+            },
+            explode("\n", $this->readText())
+        );
+    }
+}
